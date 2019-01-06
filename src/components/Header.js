cd915c0c4cdd2e73d07667e58f1a7bb9beef7282
march-dave <ysangy@gmail.com>
2019-01-06 15:06:15 -0500

    fix props

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Alert } from "reactstrap";
 
 const Header = props => {
-  const {basecurrency, currency, rate, lasttimeupdate_date} = this.props;
+  const {basecurrency, currency, rate, lasttimeupdate_date} = props;
   return (
     <h4>
       <Alert color="secondary">
